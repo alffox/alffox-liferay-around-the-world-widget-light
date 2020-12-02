b13c634015ecd1d8ea5732b2316359a2f15317d3
alffox <alfonso.crisci.it@gmail.com>
2020-12-02 12:26:32 +0100

    bug: Update footer links

--- src/modules/AtwFooterNewIssueFeatRequestButton.es.js
+++ src/modules/AtwFooterNewIssueFeatRequestButton.es.js
@@ -6,7 +6,7 @@ class AtwFooterNewIssueFeatRequestButton extends React.Component {
       <div className="col-md-4">
         <a
           className={"btn " + (this.props.isDarkMode ? 'btn-dark' : 'btn-light')}
-          href="https://loop.liferay.com/web/guest/home/-/loop/topics/_GrowFeedback"
+          href="https://liferay.slack.com/archives/CLANV2Q57"
           target="_blank"
           rel="noopener noreferrer"
         >
